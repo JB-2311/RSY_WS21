e21fb2f7a5ed68c9ddf993cfc2430a33918e9f2a
ronja221 <77384213+ronja221@users.noreply.github.com>
2021-11-30 11:13:31 +0100

    Add files via upload

--- Arbeitsraum/YB2_Pos_Bestimmung.m
+++ Arbeitsraum/YB2_Pos_Bestimmung.m
@@ -1,11 +1,14 @@
-EP_1 = [0 30 0]
-EP_2 = [0 40 0]
+EP_1 = [100 30 0]
+EP_2 = [100 40 0]
 
-s= (EP_1+EP_2)*0.5;
+s= (EP_1+EP_2)*0.5
 beta=atan2(EP_2(1),EP_2(2))
-gamma=acos(-(norm(EP_1)^2+norm(EP_2)^2+norm(EP_1-EP_2)^2)/-(2*norm(EP_2)*norm(EP_1-EP_2)))
+gamma=acos((norm(EP_1)^2-norm(EP_2)^2-norm(EP_1-EP_2)^2)/-(2*norm(EP_2)*norm(EP_1-EP_2)))
 delta=beta+gamma-pi/2;
 
-pos_YB=s+[s*cos(delta) s*sin(delta)]
+
+pos_YB=s+[norm(s)*cos(delta) norm(s)*sin(delta) 0]
+
+
 
 
